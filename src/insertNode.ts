492427d2da2f964573fb4570b4b7cf4f7316b507
Eric Liu <ericyl.us@gmail.com>
2019-09-21 07:39:18 -0700

    refactor(insert node): remove comments, extract types

--- src/insertNode.ts
+++ src/insertNode.ts
@@ -2,35 +2,24 @@ import { PostHTML } from 'posthtml';
 import parser from 'posthtml-parser';
 import { IInsertAtData } from './insertAt';
 
-function insertNode({
-  node,
-  option,
-  content
-}: {
-  node: PostHTML.Node;
-  option: IInsertAtData;
-  content: [undefined | PostHTML.RawNode[] | any];
-}): PostHTML.Node {
+function insertNode({ node, option, content }: IInsertNodeParams) {
   if (content[0]) {
     if (option.append && node.content) {
       node.content.push(parser(option.append));
-      // content = [...content, parser(option.append)] as PostHTML.Node[];
     }
 
     if (option.prepend && node.content) {
       node.content.unshift(parser(option.prepend));
-      // content = [parser(option.prepend), ...content] as PostHTML.Node[];
     }
-
-    return node;
-    /* 
-    return {
-      ...node,
-      content
-    }; */
   }
 
   return node;
+}
+
+interface IInsertNodeParams {
+  node: PostHTML.Node;
+  option: IInsertAtData;
+  content: [undefined | PostHTML.RawNode[] | unknown];
 }
 
 export { insertNode };
