d3e6d5e9095c8ffc63aba24f81903003ae65f4ec
Eric Liu <ericyl.us@gmail.com>
2019-10-12 12:03:49 -0700

    fix(insert node): remove undefined guard for node content
    Update regressed snapshot.
    
    Fixes #3 .

--- src/insertNode.ts
+++ src/insertNode.ts
@@ -2,18 +2,18 @@ import { PostHTML } from 'posthtml';
 import parser from 'posthtml-parser';
 import { IInsertAtData } from './insertAt';
 
-function insertNode({ node, option, content }: IInsertNodeParams) {
-  if (content[0]) {
-    if (option.append && node.content) {
-      node.content.push(parser(option.append));
-    }
+function insertNode({ node, option }: IInsertNodeParams) {
+  const content = node.content || [];
 
-    if (option.prepend && node.content) {
-      node.content.unshift(parser(option.prepend));
-    }
+  if (option.append) {
+    content.push(parser(option.append));
   }
 
-  return node;
+  if (option.prepend) {
+    content.unshift(parser(option.prepend));
+  }
+
+  return { ...node, content };
 }
 
 interface IInsertNodeParams {
