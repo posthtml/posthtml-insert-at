01e98aedfa59db1d4e1a9abb987762dd48d878f6
Eric Liu <ericyl.us@gmail.com>
2022-02-12 06:56:14 -0800

    chore: run prettier

--- examples/append/src/index.js
+++ examples/append/src/index.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const posthtml = require('posthtml');
-const { insertAt } = require('posthtml-insert-at');
+const fs = require("fs");
+const posthtml = require("posthtml");
+const { insertAt } = require("posthtml-insert-at");
 
-const html = fs.readFileSync('./src/index.html');
+const html = fs.readFileSync("./src/index.html");
 
 posthtml()
   .use(
     insertAt({
-      selector: '.foo',
+      selector: ".foo",
       append: `
         <footer id="footer">
           &copy; ${new Date().getFullYear()}
         </footer>
       `,
-      behavior: 'outside'
+      behavior: "outside",
     })
   )
   .process(html)
-  .then(result => fs.writeFileSync('./src/after.html', result.html));
+  .then((result) => fs.writeFileSync("./src/after.html", result.html));
